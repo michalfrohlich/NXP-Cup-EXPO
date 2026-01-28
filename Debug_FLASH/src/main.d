89b70a902c97cc4c0e86883d725f7095460d60f2
FBI <ivanfb05@gmail.com>
2026-01-28 13:10:31 +0100

    Ignore build artifacts and remove them from tracking

--- Debug_FLASH/src/main.d
+++
@@ -1,2 +0,0 @@
-src/main.o: ../src/main.c ../src/app/app_modes.h
-../src/app/app_modes.h:
--- Debug_FLASH/src/vision_linear_v2.d
+++
@@ -1,7 +0,0 @@
-src/vision_linear_v2.o: ../src/vision_linear_v2.c \
- C\:/Users/Navif/workspaceS32DS.3.6.3/NXP_Cup/include/vision_linear_v2.h \
- C\:/NXP/S32DS.3.6.3/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Platform_Types.h \
- C\:/NXP/S32DS.3.6.3/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/PlatformTypes.h
-C\:/Users/Navif/workspaceS32DS.3.6.3/NXP_Cup/include/vision_linear_v2.h:
-C\:/NXP/S32DS.3.6.3/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Platform_Types.h:
-C\:/NXP/S32DS.3.6.3/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/PlatformTypes.h:
